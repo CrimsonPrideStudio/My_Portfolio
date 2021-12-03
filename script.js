223df52a7608de12de392b2228342231bd5c47b2
sagarkumar2805 <84443519+sagarkumar2805@users.noreply.github.com>
2021-12-03 16:57:20 +0530

    cmplete

---
+++ script.js
@@ -0,0 +1,54 @@
+// sticky navigation menu
+
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+
+let val;
+
+
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("sticky");
+        scrollBtn.sytle.display = "block";
+
+    }
+    else{
+        nav.classList.remove("sticky");
+        scrollBtn.sytle.display = "none";
+        
+    }
+}
+
+// side navigation menu
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+
+
+menuBtn.onclick = function(){
+    navBar.classList.add("active");
+    menuBtn.sytle.opacity = "0";
+    menuBtn.sytle.pointerEvents = "none";
+    body.style.overflowX = "hidden";
+    scrollBtn.style.pointerEvents = "none";
+}
+
+cancelBtn.onclick = function(){
+    navBar.classList.remove("active");
+    menuBtn.sytle.opacity = "1";
+    menuBtn.sytle.pointerEvents = "auto";
+    body.style.overflowX = "auto";
+    scrollBtn.style.pointerEvents = "auto";
+}
+
+// side navigation bar close while we click on navigation links
+
+let navLinks = document.querySelectorAll(".menu li a");
+for(var i =0; i < navLinks.length; i++){
+    navLinks[i],addEventListener("click",function(){
+      navBar.classList.remove("active");
+      menuBtn.style.opacity="1";
+      menuBtn.sytle.pointerEvents = "auto";
+})
+}
